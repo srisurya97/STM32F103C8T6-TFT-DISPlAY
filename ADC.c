7de78359e8cd99a1931f8037b3dde5e3fabb23a6
srisurya97 <srisuryav2010@gmail.com>
2020-05-16 10:49:21 +0530

    Improved UI for VOltmeter

--- ADC.c
+++ ADC.c
@@ -2,6 +2,7 @@
 #include "stm32f10x.h"
 #include "delay.h"
 #include "lcd.h"
+#include "MenuFramework.h"
 
 //A4,A5,A6,A7
 
@@ -12,20 +13,18 @@ void adcinit(void)
 	//A5 Pin Config
 	GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5_0); //Input mode
 	GPIOA->CRL |= GPIO_CRL_CNF5_1; //Input PushPull
-	
+		
 	ADC1->CR1 |=ADC_CR1_EOCIE; //Enable Interrupt
 	ADC1->CR2 |= ADC_CR2_CONT; //Set to Continous Mode
 	ADC1->CR2 |= ADC_CR2_ALIGN; //Left Align
 	ADC1->SMPR2 |= ADC_SMPR2_SMP5; //Sampling rate in channel 5 to 239 cycles
-	//ADC1->SQR1 &= ~ADC_SQR1_L; //Only 1 Conversion
-	ADC1->SQR3 |= ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_2; //5th Channel
+	ADC1->SQR3 |= ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_2; //5th Channel,
 	NVIC_EnableIRQ(ADC1_2_IRQn);
 	
 	ADC1->CR2 |= ADC_CR2_ADON;
 	delay_ms(1);
 	ADC1->CR2 |= ADC_CR2_CAL;
 	while ((ADC1->CR2 & ADC_CR2_CAL)==1);
-	//ADC1->CR2 |= ADC_CR2_ADON;	
 }
 
 uint16_t ADCConvert (void)
@@ -34,38 +33,45 @@ uint16_t ADCConvert (void)
 }
 
 
-void graph (uint16_t graphdata)
+void graph (uint16_t graphdata,uint8_t mode) 
 {
 	static uint16_t x=22, preval=315,drawdata=0;
 	uint16_t y=315;
 	POINT_COLOR=WHITE;
 	LCD_DrawRectangle(22,30,238,318);
-	LCD_ShowString(1,30,30,20,12,"0V-");
-	LCD_ShowString(1,73,30,20,12,"0.5");
-	LCD_ShowString(1,116,30,20,12,"1V-");
-	LCD_ShowString(1,159,30,20,12,"1.5");
-	LCD_ShowString(1,202,30,20,12,"2V-");
-	LCD_ShowString(1,245,30,20,12,"2.5");
-	LCD_ShowString(1,288,30,20,12,"3V-");
-	LCD_ShowString(1,307,30,12,12,"3.3");
-	
+	POINT_COLOR=GREEN;
+	LCD_ShowString(1,27,30,20,12,"3.3");
+	LCD_ShowString(1,49,30,20,12,"3V-");
+	LCD_ShowString(1,92,30,20,12,"2.5");
+	LCD_ShowString(1,135,30,20,12,"2V-");
+	LCD_ShowString(1,178,30,20,12,"1.5");
+	LCD_ShowString(1,221,30,20,12,"1V-");
+	LCD_ShowString(1,264,30,20,12,"0.5");
+	LCD_ShowString(1,307,30,12,12,"0V-");
 	
 	if(x >= 237)
 		{
 			LCD_Fill(22,31,237,317,BLACK);
 			x=22;
 	  }
-	
-		
 	drawdata = graphdata/231;
-	
-			for(y=316;y>=(drawdata+34);y--)
+	POINT_COLOR=BLUE;
+	if(mode == 0)
+		{
+			for(y=316;y>=(316-drawdata);y--)
+				{
+					LCD_DrawPoint(x,y);
+				}
+		}
+		else
 			{
-				LCD_DrawPoint(x,y);
+						LCD_DrawPoint(x,(316-drawdata));
 			}
-		
-		
-	x= x+2; 		
+	if(mode == 0)
+		{
+			x= x+2; 		
+		}
+	else { x++; }		
 }  
 
 
@@ -73,14 +79,9 @@ void ADC1_2_IRQHandler(void)
 {
 	uint16_t Data=0;
 	if(ADC1->SR & ADC_SR_EOC)
-		{
-			POINT_COLOR = WHITE;
-			LCD_ShowString(50,17,120,20,12,"ADC Value ");
+		{			
 			Data = ADC1->DR; 	
-  		POINT_COLOR = BLACK;
-			LCD_ShowNum(110,17,Data,6,12);	
-			graph(Data);
-			
+			graph(Data,1);
 		}
 		else{
 			POINT_COLOR =RED;
